75fece381d61eef2f7647c687af35c0b1315353f
ufobsk <igoico10@gmail.com>
2023-07-23 19:40:31 -0300

    first-commit

--- main.js
+++ main.js
@@ -8,21 +8,29 @@ class ProductManager {
   addProducts(tittle, description, price, thumbnail, code, stock) {
     for (let i = 0; i < this.products.length; i++) {
       if (this.products[i].code === code) {
-        console.log(`EL codigo ${code} esta repetido`);
+        console.log(`EL codigo "${code}" esta repetido`);
         break;
       }
     }
 
-    ProductManager.id++;
-    this.products.push({
+    const newProduct = {
       tittle,
       description,
       price,
       thumbnail,
       code,
       stock,
-      id: ProductManager.id,
-    });
+    };
+
+    if (!Object.values(newProduct).includes(undefined)) {
+      ProductManager.id++;
+      this.products.push({
+        ...newProduct,
+        id: ProductManager.id,
+      });
+    } else {
+      console.log("No se han completado todos los campos");
+    }
   }
 
   getProduct() {
@@ -33,6 +41,8 @@ class ProductManager {
     return this.products.find((producto) => producto.id === id);
   }
 
+
+  //Método que busca el producto que coincida con el id
   getProductById(id) {
     !this.existe(id) ? console.log("Not Found") : console.log(this.existe(id));
   }
@@ -40,11 +50,16 @@ class ProductManager {
 
 const productos = new ProductManager();
 
+//Agregamos productos
 productos.addProducts("Termo", "Un buen termo", 8000, "img1", "code1", 7);
 productos.addProducts("Mate", "Un buen mate", 5000, "img2", "code2", 7);
 
-
+//Mostramos el rreglo con los productos
 console.log(productos.getProduct());
 
-productos.addProducts("Bobilla", "Una buena bombilla", 1000, "img3", "code2", 10);
-//productos.getProductById(2);
+//Verifica que el codigo esta repetido
+productos.addProducts("Bobilla", "Una buena bombilla", 1000, "img3", "code2", 10); //El codigo "code2" esta repetido
+
+//Verificar busqueda por id / Not Found si no existe
+productos.getProductById(2); //Muesta el prodcuto que contiene el id 2
+productos.getProductById(4); //Not Found
